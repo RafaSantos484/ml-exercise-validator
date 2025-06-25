8f67bcd2e2ad3281193e213bddcfe0dae5de0a54
rafael.santos <rafaelsantos32141@gmail.com>
2025-06-25 18:09:03 -0300

    Conversão de modelos SVM e Logistic Regression para usar formato .onnx

--- src/classes/models/logistic-regression.ts
+++ src/classes/models/logistic-regression.ts
@@ -1,49 +1,21 @@
-import type { Landmark } from "@mediapipe/tasks-vision";
 import { Model } from "./model.class";
+import type { Landmark } from "@mediapipe/tasks-vision";
+import { type InferenceSession } from "onnxruntime-web";
 import Utils from "../utils.class";
-import Point3d from "../point3d.class";
 
-type LogisticRegressionParams = {
-  C: number;
-  dual: boolean;
-  fit_intercept: boolean;
-  max_iter: number;
-  penalty: null | "l1" | "l2" | "elasticnet";
-  solver:
-    | "lbfgs"
-    | "liblinear"
-    | "newton-cg"
-    | "newton-cholesky"
-    | "sag"
-    | "saga";
-};
-type LogisticRegressionModelData = { coef: number[][]; intercept: number[] };
+export class LogisticRegressionModel extends Model {
+  protected modelPath =
+    "/models/high-plank/logistic-regression/full_body_model.onnx";
 
-export class LogisticRegressionModel extends Model<
-  LogisticRegressionParams,
-  LogisticRegressionModelData
-> {
-  private logisticPredict(x: number[]): [number, number] {
-    const { coef, intercept } = this.modelJson.model_data;
-    const logits = coef.map((weights, i) =>
-      weights.reduce((sum, wj, j) => sum + wj * x[j], intercept[i])
-    );
-    const probs =
-      coef.length === 1
-        ? [1 - Utils.sigmoid(logits[0]), Utils.sigmoid(logits[0])]
-        : Utils.softmax(logits);
-
-    const maxIdx = probs.indexOf(Math.max(...probs));
-    return [maxIdx, probs[maxIdx]];
-  }
-
-  predict(landmarks: Landmark[]): string {
-    const x = this.modelJson.features.angles.map((triplet) =>
-      Point3d.getAngleFromPointsTriplet(landmarks, triplet)
-    );
-    const [prediction, prob] = this.logisticPredict(x);
-    const label = this.modelJson.classes[prediction];
-    const translatedLabel = Utils.translate(label);
-    return `${translatedLabel}(${prob.toFixed(2)})`;
+  async predict(landmarks: Landmark[]): Promise<string> {
+    const tensor = this.getTensor(landmarks);
+    const session = await this.getSession();
+    const feeds: InferenceSession.FeedsType = {
+      [session.inputNames[0]]: tensor,
+    };
+    const results = await session.run(feeds);
+    const output = results[session.outputNames[0]];
+    const label = output.data[0] as string;
+    return Utils.translate(label);
   }
 }
--- src/classes/models/model-factory.class.ts
+++ src/classes/models/model-factory.class.ts
@@ -2,12 +2,16 @@ import type { Exercise } from "../../types";
 import { type Classifier } from "./model.class";
 import { KnnModel } from "./knn.class";
 import { RandomForestModel } from "./random-forest.class";
+import { SvmModel } from "./svm.class";
+import { LogisticRegressionModel } from "./logistic-regression";
 
 export class ModelFactory {
   private static models: Record<Exercise, Record<string, Classifier>> = {
     high_plank: {
       KNN: new KnnModel(),
       "Random Forest": new RandomForestModel(),
+      "Regressão Logística": new LogisticRegressionModel(),
+      SVM: new SvmModel(),
     },
   };
 
